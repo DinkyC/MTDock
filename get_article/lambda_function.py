74a93500020c4d8dc7f5892523565963caf42ec3
daniel <kb1ubp@gmail.com>
2023-10-13 19:33:48 -0400

    created classes for get_artilce / included get_final and get_first

--- get_article/lambda_function.py
+++ get_article/lambda_function.py
@@ -1,51 +1,82 @@
 import pymysql
 import logging
 import traceback
-from os import environ
+import os 
 import json
-
-endpoint=environ.get('ENDPOINT')
-port=environ.get('PORT')
-dbuser=environ.get('DBUSER')
-password=environ.get('DBPASSWORD')
-database=environ.get('DATABASE')
-
 
 logger=logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def construct_query(title=None, id=None):
-    base_query = "SELECT id, title, BodyText FROM HighTimes"
-    conditions = []
-    params = []
-    
-    if title:
-        conditions.append("title = %s")
-        params.append(title)
-    if id:
-        conditions.append("id = %s")
-        params.append(id)
-    
-    if conditions:
-        base_query += " WHERE " + " AND ".join(conditions)
-    
-    base_query += " LIMIT 1"
-    
-    return base_query, params
+class HTDatabase:
+    def construct_query(title=None, id=None):
+        base_query = "SELECT id, title, BodyText FROM HighTimes"
+        conditions = []
+        params = []
+        
+        if title:
+            conditions.append("title = %s")
+            params.append(title)
+        if id:
+            conditions.append("id = %s")
+            params.append(id)
+        
+        if conditions:
+            base_query += " WHERE " + " AND ".join(conditions)
+        
+        base_query += " LIMIT 1"
+        
+        return base_query, params
 
 
-def make_connection():
-    return pymysql.connect(host=endpoint, user=dbuser, passwd=password,
-        port=int(port), db=database, autocommit=True)
+    def get_database_credentials(self):
+        secret = self.get_secret()
+        secret_dict = json.loads(secret)
+        return secret_dict.get('username'), secret_dict.get('port'), secret_dict.get('database'), secret_dict.get('host')
 
-def log_err(errmsg):
-    logger.error(errmsg)
-    return {"body": errmsg , "headers": {}, "statusCode": 400,
-        "isBase64Encoded":"false"}
+
+    def get_secret(self):
+        secret_name = "HighTimesDB"
+        region_name = os.environ['AWS_REGION'] 
+        session = boto3.session.Session()
+        client = session.client(service_name='secretsmanager', region_name=region_name)
+        try:
+            get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+            return get_secret_value_response['SecretString']
+        except Exception as e:
+            raise e
+
+    def make_connection(self):
+        rds_client = boto3.client('rds')
+
+        username, port, database, endpoint = self.get_database_credentials()
+
+        database_token = rds_client.generate_db_auth_token(
+            DBHostname=endpoint,
+            Port=port,
+            DBUsername=username,
+            Region=os.environ['AWS_REGION']
+            )
+
+        return pymysql.connect(
+            host=endpoint,
+            user=username,
+            passwd=database_token,
+            port=int(port),
+            db=database,  
+            autocommit=True,
+            ssl={'ssl': True}
+        )
+
+    def log_err(errmsg):
+        logger.error(errmsg)
+        return {"body": errmsg , "headers": {}, "statusCode": 400,
+            "isBase64Encoded":"false"}
 
 logger.info("Cold start complete.") 
 
 def handler(event,context):
+    db = HTDatabase()
+
     queryStringParameters = event.get("queryStringParameters", {})
 
     # Extract values or set to None if not provided
@@ -58,23 +89,23 @@ def handler(event,context):
         logger.info("No title provided.")
 
     try:
-        query, params = construct_query(title=title, id=id)
+        query, params = db.construct_query(title=title, id=id)
     except Exception as e:
-        logger.error(f"ERROR: Cannot construct query. {e}")
+        db.logger.error(f"[ERROR]: Cannot construct query. {e}")
         return {
             "statusCode": 500,
-            "body": f"ERROR: Cannot construct query.\n{str(e)}"
+            "body": f"[ERROR]: Cannot construct query.\n{str(e)}"
         }
 
 
     try:
-        cnx = make_connection()
+        cnx = db.make_connection()
         cursor=cnx.cursor()
         
         try:
             cursor.execute(query, params)
         except:
-            return log_err ("ERROR: Cannot execute cursor.\n{}".format(
+            return db.log_err ("[ERROR]: Cannot execute cursor.\n{}".format(
                 traceback.format_exc()) )
 
     
@@ -82,7 +113,7 @@ def handler(event,context):
             result = cursor.fetchall()
             cursor.close()
         except:
-            return log_err("ERROR: Cannot retrieve query data.\n{}".format(
+            return db.log_err("[ERROR]: Cannot retrieve query data.\n{}".format(
                 traceback.format_exc()))
         # If there's a result, process it
         if result:
@@ -104,7 +135,7 @@ def handler(event,context):
         }
 
     except:
-        return log_err("ERROR: Cannot connect to database from handler.\n{}".format(
+        return db.log_err("[ERROR]: Cannot connect to database from handler.\n{}".format(
             traceback.format_exc()))
 
 
