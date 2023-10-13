db53ffe782a15a703204fcf53f046ca764f51c5a
daniel <kb1ubp@gmail.com>
2023-10-13 14:15:24 -0400

    RDS Proxy

--- put_final_translation/lambda_function.py
+++ put_final_translation/lambda_function.py
@@ -1,60 +1,79 @@
 import pymysql
 import logging
 import traceback
-from os import environ
+import os
 import hashlib
 import boto3
 import json
 
-port = environ.get('PORT')
-database = environ.get('DATABASE')
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-def get_database_credentials():
-    secret = get_secret()
-    secret_dict = json.loads(secret)
-    return secret_dict.get('username'), secret_dict.get('password'), secret_dict.get('host')
-
-
-def get_secret():
-    secret_name = "HighTimesDB"
-    region_name = "us-west-1"
-    session = boto3.session.Session()
-    client = session.client(service_name='secretsmanager', region_name=region_name)
-    try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-        return get_secret_value_response['SecretString']
-    except Exception as e:
-        raise e
-
-def make_connection():
-    dbuser, password, endpoint = get_database_credentials()
-    return pymysql.connect(
-        host=endpoint,
-        user=dbuser,
-        passwd=password,
-        port=int(port),
-        db=database,  
-        autocommit=True
-    )
+class HTDatabase:
+    def get_database_credentials(self):
+        secret = self.get_secret()
+        secret_dict = json.loads(secret)
+        return secret_dict.get('username'), secret_dict.get('port'), secret_dict.get('database'), secret_dict.get('host')
 
 
-def log_err(errmsg):
-    logger.error(errmsg)
-    return {"body": errmsg, "headers": {}, "statusCode": 400, "isBase64Encoded": "false"}
+    def get_secret(self):
+        secret_name = "HighTimesDB"
+        region_name = "us-west-1"
+        session = boto3.session.Session()
+        client = session.client(service_name='secretsmanager', region_name=region_name)
+        try:
+            get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+            return get_secret_value_response['SecretString']
+        except Exception as e:
+            raise e
 
-def compute_checksum(data):
-    return hashlib.sha256(str(data).encode('utf-8')).digest()
+    def make_connection(self):
+        rds_client = boto3.client('rds')
+
+        username, port, database, endpoint = self.get_database_credentials()
+
+        database_token = rds_client.generate_db_auth_token(
+            DBHostname=endpoint,
+            Port=port,
+            DBUsername=username,
+            Region=os.environ['AWS_REGION']
+            )
+
+        return pymysql.connect(
+            host=endpoint,
+            user=username,
+            passwd=database_token,
+            port=int(port),
+            db=database,  
+            autocommit=True
+        )
+
+
+    def log_err(self, errmsg):
+        logger.error(errmsg)
+        return {"body": errmsg, "headers": {}, "statusCode": 400, "isBase64Encoded": "false"}
+
+    def compute_checksum(self, data):
+        return hashlib.sha256(str(data).encode('utf-8')).digest()
+
 
 def handler(event, context):
+    db = HTDatabase()
+
     try:
         # Extract the data you want to insert from the event or any other source
-        translated_data = {"title": event.get("title"), "text": event.get("text"), "id": event.get("id"), "comments": event.get("comments", None)}
-        checksum = compute_checksum(translated_data)
-        cnx = make_connection()
+        translated_data = {"title": event.get("title"), "text": event.get("text"), "id": event.get("id")}
+        
+        checksum = db.compute_checksum(translated_data)
+        cnx = db.make_connection()
         cursor = cnx.cursor()
+        
+        if checksum.hex() != event.get('checksum'):
+            return {
+                    "statusCode": 500,
+                    "body": "Data corruption",
+                    "isBase64Encoded": "false"
+
+                    } 
 
         try:
             # Check if the article already exists based on id and checksum
@@ -62,7 +81,7 @@ def handler(event, context):
                 SELECT COUNT(*) FROM HighTimes.final_translation
                 WHERE id = %s AND checksum = %s
             """
-            cursor.execute(select_statement, (translated_data["id"], checksum))
+            cursor.execute(select_statement, (event.get('id'), checksum))
             result = cursor.fetchone()
 
             # If a row with the same id and checksum is found, it means the article already exists
@@ -76,27 +95,38 @@ def handler(event, context):
 
             # Create an INSERT statement
             insert_statement = """
-            INSERT INTO final_translation (id, title, BodyText, comments, checksum) 
-            VALUES (%s, %s, %s, %s, %s)
+            INSERT INTO final_translation (id, title, BodyText, comments, rating, checksum) 
+            VALUES (%s, %s, %s, %s, %s, %s)
             """
-            
-            # Execute the INSERT statement with the data
-            cursor.execute(insert_statement, (event.get("id"), event.get("title"), event.get("text"), event.get("comments", None), checksum))
+            try:
+                # Execute the INSERT statement with the data
+                cursor.execute(insert_statement, (event.get("id"), event.get("title"), event.get("text"), event.get("comments", None), event.get('rating'), checksum))
+            except Exception as e:
+                return {
+                    "statusCode": 500,
+                    "body": "Element not passed in",
+                    "isBase64Encoded": "false"
+                }
 
         except Exception as e:
-            return log_err("ERROR: Cannot execute INSERT statement.\n{}".format(traceback.format_exc()))
+            return db.log_err("ERROR: Cannot execute INSERT statement.\n{}".format(traceback.format_exc()))
 
         return {"body": "Data inserted successfully", "headers": {}, "statusCode": 200, "isBase64Encoded": "false"}
 
     except Exception as e:
-        return log_err("ERROR: Cannot connect to the database from the handler.\n{}".format(traceback.format_exc()))
+        return db.log_err("ERROR: Cannot connect to the database from the handler.\n{}".format(traceback.format_exc()))
 
     finally:
         try:
             cnx.close()
         except:
             pass
-
 if __name__ == "__main__":
-    handler(None, None)
-
+    event = {
+  "title": "value1",
+  "text": "value2",
+  "id": 57,
+  "checksum":"c0c4208611a66297644373675e64c20e81d43b1760ab40a42605b6bd7908912c",
+  "rating": 4
+}
+    handler(event, None) 
